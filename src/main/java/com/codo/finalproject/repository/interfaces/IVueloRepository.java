f580c3a6b138effa570c4dccbf95a224cdd3a70b
Leandro-Aballone <l.aballone@gmail.com>
2023-12-05 03:27:31 -0300

    Se incorpora metodo findByNotEmpty en IVueloRepository

--- src/main/java/com/codo/finalproject/repository/interfaces/IVueloRepository.java
+++ src/main/java/com/codo/finalproject/repository/interfaces/IVueloRepository.java
@@ -2,6 +2,18 @@ package com.codo.finalproject.repository.interfaces;
 
 import com.codo.finalproject.entity.Vuelo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface IVueloRepository extends JpaRepository<Vuelo,Long> {
+    //List<Vuelo>FindByisFullFalse();
+
+    @Query(value = "SELECT * FROM flysky.vuelo WHERE is_full = 0",nativeQuery = true)
+    List<Vuelo>findByNotEmpty();
+
+
+
 }
