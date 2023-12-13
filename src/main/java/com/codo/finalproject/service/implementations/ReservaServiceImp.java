887aa9ec657cbd936aac6fb979d6839177439332
Leandro-Aballone <l.aballone@gmail.com>
2023-12-13 02:52:37 -0300

    Alternativa para getTopDestinationByUser

--- src/main/java/com/codo/finalproject/service/implementations/ReservaServiceImp.java
+++ src/main/java/com/codo/finalproject/service/implementations/ReservaServiceImp.java
@@ -2,35 +2,34 @@ package com.codo.finalproject.service.implementations;
 
 import com.codo.finalproject.dto.request.PagoDto;
 import com.codo.finalproject.dto.request.ReservaDto;
-import com.codo.finalproject.dto.request.VueloDto;
 import com.codo.finalproject.dto.response.ResponseDto;
-import com.codo.finalproject.dto.response.HistorialReservaPorUsuarioDto;
 import com.codo.finalproject.dto.response.TopDestinoDto;
-import com.codo.finalproject.dto.request.idUsuarioDto;
 import com.codo.finalproject.entity.Comprobante;
 import com.codo.finalproject.entity.Reserva;
 import com.codo.finalproject.exception.ReservaNotFoundException;
 import com.codo.finalproject.exception.TopDestinoNotFoundException;
 import com.codo.finalproject.repository.interfaces.IComprobanteRepository;
 import com.codo.finalproject.repository.interfaces.IReservaRepository;
+import com.codo.finalproject.repository.interfaces.IUsuarioRepository;
 import com.codo.finalproject.service.interfaces.IReservaService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
-import com.codo.finalproject.exception.ReservaNotFoundException;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class ReservaServiceImp implements IReservaService {
     private final IReservaRepository reservaRepository;
+    private final IUsuarioRepository usuarioRepository;
     private final IComprobanteRepository comprobanteRepository;
     private final ObjectMapper mapper;
 
-    public ReservaServiceImp(IReservaRepository reservaRepository, IComprobanteRepository comprobanteRepository){
+    public ReservaServiceImp(IReservaRepository reservaRepository, IUsuarioRepository usuarioRepository, IComprobanteRepository comprobanteRepository){
         mapper = new ObjectMapper();
         this.reservaRepository = reservaRepository;
         this.comprobanteRepository = comprobanteRepository;
+        this.usuarioRepository = usuarioRepository;
+
     }
 
     @Override
@@ -76,6 +75,42 @@ public class ReservaServiceImp implements IReservaService {
                         dto.getDestino(),
                         dto.getCantidadReservas()))
                 .toList();
+    }
+
+    public List<TopDestinoDto> getTopDestinationByUser(Long userId){
+        int rankingSize = 3; //TODO: Agregar variable al endpoint
+
+        List<Reserva> reservas = reservaRepository.findAllById(Collections.singleton(userId));
+        String username = usuarioRepository.findById(userId).orElseThrow().getNombre();
+        if (reservas.isEmpty()) throw new TopDestinoNotFoundException("No se encontro ningun destino para el usuario: "+ username);
+        TreeMap<String,Long>destinos = new TreeMap<>();
+        reservas.stream().map(r -> r.getVuelo_reserva().getAeropuertoDestino()).forEach(
+                v -> {
+                    if(destinos.containsKey(v)) {
+                        destinos.put(v, destinos.get(v) + 1);
+                    }else {
+                        destinos.put(v, 1L);
+                    }
+                }
+        );
+
+        return rankingDestinosPopularesCalc(destinos,rankingSize);
+    }
+
+    private List<TopDestinoDto> rankingDestinosPopularesCalc(TreeMap<String,Long> lista, int rankingSize){
+        List<TopDestinoDto> destinoDtoList = new ArrayList<>();
+        if(rankingSize <= 0)return destinoDtoList;
+        while (rankingSize != 0){
+            Long max = Collections.max(lista.values());
+            lista.forEach((s,l)->{
+                if(l.equals(max)){
+                    destinoDtoList.add(new TopDestinoDto(s,l));
+                    lista.remove(s,l);
+                }
+            });
+            rankingSize--;
+        }
+        return destinoDtoList;
     }
 
 }
