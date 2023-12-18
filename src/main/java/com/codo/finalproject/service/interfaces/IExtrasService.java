575c40b02c59226ae2e60a2358e5a979e10fb527
Leandro-Aballone <l.aballone@gmail.com>
2023-12-17 21:08:21 -0300

    ExtrasController ahora se encuentra en la rama dataGen

--- src/main/java/com/codo/finalproject/service/interfaces/IExtrasService.java
+++
@@ -1,8 +0,0 @@
-package com.codo.finalproject.service.interfaces;
-
-import com.codo.finalproject.dto.response.ResponseDto;
-
-public interface IExtrasService {
-
-    ResponseDto cargarBaseDeDatos(int cantidad);
-}
